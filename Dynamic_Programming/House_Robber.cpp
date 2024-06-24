4ea690e22ed2d098861c1010c37bf425ed371485
Shashwat Dixit <75736156+shashwatdixit10@users.noreply.github.com>
2024-06-25 03:06:15 +0530

    Create House_Robber.cpp

---
+++ Dynamic_Programming/House_Robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n  = nums.size();
+        vector<int> dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i=2;i<=n;i++){
+            int steal = nums[i-1] + dp[i-2];
+            int skip = dp[i-1];
+            dp[i] = max(steal,skip);
+        }
+        return dp[n];
+    }
+};
