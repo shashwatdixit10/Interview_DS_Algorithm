9e00596bd66ee023be24465bc9d0431a695dde0f
Shashwat Dixit <75736156+shashwatdixit10@users.noreply.github.com>
2024-06-26 02:38:00 +0530

    Update find_all_numbers_disappeared_in_an_array.cpp

--- ARRAYS/Hash_Table/find_all_numbers_disappeared_in_an_array.cpp
+++ ARRAYS/Hash_Table/find_all_numbers_disappeared_in_an_array.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+       unordered_map<int,int> mp;
+       for(int i=1;i<=nums.size();i++){
+        mp[i]++;
+       }
 
+       for(int i=0;i<nums.size();i++){
+        if(mp.find(nums[i]) != mp.end())
+        mp[nums[i]]++;
+       }
+       for(auto it : mp){
+        if(it.second < 2 )
+        ans.push_back(it.first);
+       }
+return ans;
+    }
+};
